c16e4be0cfa5f3405508d206209134c6007b2e59
adedola12 <dolapo836@gmail.com>
2025-04-17 13:29:20 +0100

    routes created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Collection from './pages/Collection'
+import Content from './pages/Content'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'> 
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/collection' element={<Collection />} />
+          <Route path='/content' element={<Content />} />
+          <Route path='Order' element={<Orders />} />
+          <Route path='/placeOrder' element={<PlaceOrder />} />
+          <Route path='/product' element={<Product />} />
+          <Route path='/collection/:id' element={<Collection />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,31 @@
+import bin_icon from './bin.png';
+import call_icon from './call.png';
+import cart_icon from './cart.png';
+import close_icon from './close.png';
+import color_logo from './color.png';
+import logo_black from './Logo-black.png';
+
+
+export const assets = {
+  bin_icon,
+  call_icon,
+  cart_icon,
+  close_icon,
+  color_logo,
+  logo_black
+}
+
+export const products = [
+    {
+        _id: "aaaa",
+        name: "Product 1",
+        description: "Description for product 1",
+        price: 100,
+        image: [p_img1],
+        category: "Category 1",
+        subCategory: "Subcategory 1",
+        size: ['S', 'M', 'L'],
+        date: "2023-10-01",
+        bestseller: true,
+    }
+]
